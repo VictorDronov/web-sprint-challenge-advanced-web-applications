cee746ffbbdce16a15d3c04cad95b14da0447b0c
victor dronov <victordronov1@gmail.com>
2020-09-18 11:36:49 -0400

    axios with auth orking and login working

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Pack } from "@potion/layout";
 import { Svg, Circle } from "@potion/element";
-
+import { Link } from "react-router-dom";
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
   useEffect(() => {
     const generateBubbleData = colors.map((_, i) => ({
       value: Math.floor(Math.random() * (colors.length * 2)) + 1,
-      key: `${i + 1}`
+      key: `${i + 1}`,
     }));
     setBubbleData(generateBubbleData);
   }, [colors]);
@@ -15,18 +15,19 @@ const Bubbles = ({ colors }) => {
   return (
     <div className="bubble-wrap">
       <p>bubbles</p>
+      <Link to="/">home</Link>
       <Svg width={400} height={400}>
         <Pack
           data={{
-            children: bubbleData
+            children: bubbleData,
           }}
-          sum={datum => datum.value}
+          sum={(datum) => datum.value}
           size={[400, 400]}
           includeRoot={false}
-          nodeEnter={d => ({ ...d, r: 0 })}
+          nodeEnter={(d) => ({ ...d, r: 0 })}
           animate
         >
-          {nodes =>
+          {(nodes) =>
             nodes
               .map(({ x, y, r, key }, i) => {
                 if (i < colors.length) {
@@ -42,7 +43,7 @@ const Bubbles = ({ colors }) => {
                 }
                 return null;
               })
-              .filter(v => v)
+              .filter((v) => v)
           }
         </Pack>
       </Svg>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const initialcredentials = {
+  username: "",
+  password: "",
+};
 
 const Login = () => {
+  const [credentials, setCredentials] = useState(initialcredentials);
+  const history = useHistory();
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const change = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post("/api/login", credentials)
+      .then((res) => {
+        console.log(res)
+        window.localStorage.setItem("token", res.data.payload)
+        history.push("/protected")
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login} >
+        <label>
+          Username:
+          <input type="text" name="username" onChange={change} />
+        </label>
+        <label>
+          <label>
+            Password:
+            <input type="password" name="password" onChange={change} />
+          </label>
+        </label>
+        <button>Log In</button>
+      </form>
     </>
   );
 };
